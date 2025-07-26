f056d4563a8b1b9796d149b25ac02556352fbcff
Gaddi <gaddielbarrios444@gmail.com>
2025-07-25 18:50:22 -0700

    new see password button in login html

--- screens/login/code.js
+++ screens/login/code.js
@@ -61,6 +61,18 @@ export function init() {
         showToast("Unexpected error. Please try again.", "error");
       }
     });
+
+  document
+    .getElementById("togglePassword")
+    .addEventListener("click", function () {
+      const passwordInput = document.getElementById("password");
+      const type =
+        passwordInput.getAttribute("type") === "password" ? "text" : "password";
+      passwordInput.setAttribute("type", type);
+
+      this.classList.toggle("fa-eye");
+      this.classList.toggle("fa-eye-slash");
+    });
 }
 
 function isWebDevice() {
